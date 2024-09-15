e17072dd0ae3c63ad8f3aed8841b463bd185831f
raphsenn <raphaelsenn@outlook.de>
2024-09-15 20:34:06 +0200

    one more test

--- tests/test_markov_next_word.py
+++ tests/test_markov_next_word.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
+# Author: Raphael Senn
 
 from src.markov_next_word import MarkovNextWord
 
-
-def test_next_word_prediction():
+def test_train() -> None:
     mnw = MarkovNextWord()
-    mnw.train("tests/test.txt")
+    mnw.train("data/test.txt")
+    assert mnw.word_to_nextwords == {'i': ['like', 'like', 'love'], 'like': ['photography', 'science'], 'love': ['mathematics']}
+
+def test_next_word_prediction() -> None:
+    mnw = MarkovNextWord()
+    mnw.train("data/test.txt")
     assert mnw.predict_next_words('I') == [('like', 0.6666666666666666), ('love', 0.3333333333333333)]
     assert mnw.predict_next_words('i') == [('like', 0.6666666666666666), ('love', 0.3333333333333333)]
     assert mnw.predict_next_words('like') == [('photography', 0.5), ('science', 0.5)]
