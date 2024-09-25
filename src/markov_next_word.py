cb7cb3e924794294b4900bb85a45ccdbd90f98c3
raphsenn <raphaelsenn@outlook.de>
2024-09-25 19:19:14 +0200

    shakespeare'

--- src/markov_next_word.py
+++ src/markov_next_word.py
@@ -29,7 +29,7 @@ class MarkovNextWord:
                     self.word_to_next_word_prob[(word, next_word)] = 1.0 / num_word_to_next_words
                 else: self.word_to_next_word_prob[(word, next_word)] += 1.0 / num_word_to_next_words
 
-    def predict_next_words(self, word: str) -> list[tuple[str, float]]:
+    def predict_next_words(self, word: str, top_k: int=5) -> list[tuple[str, float]]:
         result = [] 
         word = word.lower() 
         if word in self.word_to_nextwords:
@@ -37,17 +37,22 @@ class MarkovNextWord:
             for next_word in next_words:
                     result.append((next_word, self.word_to_next_word_prob[(word, next_word)]))
             result = sorted(result, key=lambda x: x[1], reverse=True)
-        return result
+        return result[:top_k]
     
-    def generate_text(self, input_text: str, seq_len: int=50) -> None:
+    def generate_text(self, input_text: str, seq_len: int=50, temperature: float=1.0, top_k: int=5) -> None:
         input_text_tokenized = tokenize_words(input_text)
         last_word = input_text_tokenized[-1]
         print(" ".join(input_text_tokenized), end=' ')
+        
         for _ in range(seq_len):
-            next_words = self.predict_next_words(last_word)
+            next_words = self.predict_next_words(last_word, top_k)
             if len(next_words) > 0:
                 words, probs = zip(*next_words)
-                last_word = np.random.choice(words, p=probs)
+
+                # Temperatur scaling.
+                scaled_props = np.array(probs) ** (1.0 / temperature)
+                scaled_props /= np.sum(scaled_props) 
+                last_word = np.random.choice(words, p=scaled_props)
                 print(last_word, end=' ')
             else:
                 break
